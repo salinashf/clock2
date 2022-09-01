12f6bfeb7821c974dbfc705fb2f797219ce7e98e
Salinas Flores Henry <salinashf@gmail.com>
2022-09-01 00:52:52 -0500

    relog

---
+++ lib/src/app.dart
@@ -0,0 +1,28 @@
+import 'package:clock2/src/pages/tab/tab_bar_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'theme/theme_service.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    ThemeService().initBrighness();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarLayout();
+  }
+}
